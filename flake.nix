314ab14b7ec9c8a5ec4c6c376e5ff9a355c65647
A.Nazary <ashkan.kh.nazary@gmail.com>
2025-08-08 23:10:15 +0200

    nix flake can do `stack run memory`

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+            ghc = pkgs.haskell.compiler.ghc948;
+            haskellPackages = pkgs.haskellPackages.override { inherit ghc; };
+            clangGcc = pkgs.runCommand "clangGcc" {} ''
+                mkdir -p $out/bin
+                ln -s ${pkgs.clang}/bin/clang $out/bin/gcc
+            '';
+        in { 
+            devShells.default = pkgs.mkShell { 
+                buildInputs = [
+                    pkgs.git
+                    pkgs.stack
+                    clangGcc
+                    pkgs.vulkan-headers
+                    pkgs.vulkan-loader
+                ];
+            };
+        });
+}

1dad7d68f2584884d55542b7e27f534166ebbb40
A.Nazary <ashkan.kh.nazary@gmail.com>
2025-11-29 22:51:08 +0100

    Better names

--- src/Node.hs
+++ src/Node.hs
@@ -1,14 +1,12 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE StrictData #-}
 
 module Node
   ( Tree,
-    node0,
     node,
     tree0,
-    tree,
+    tree
   )
 where
 
@@ -17,21 +15,18 @@ import Measure (PixelVec)
 import Render (Render (..))
 import Vertex (Vertex, applyVert)
 
-data Tree = forall a. (Render a) => Leaf a | Parent Affine [Tree]
+data Tree = forall a. (Render a) => Leaf a | Tree Affine [Tree]
 
 instance Render Tree where
   render :: Tree -> [Vertex PixelVec]
   render (Leaf a) = render a
-  render (Parent tr a) = applyVert tr <$> (render =<< a)
-
-node0 :: (Render a) => a -> Tree
-node0 = Leaf
+  render (Tree tr a) = applyVert tr <$> (render =<< a)
 
 tree0 :: [Tree] -> Tree
 tree0 = tree mempty
 
 node :: (Render c) => Affine -> c -> Tree
-node tr c = tree tr [node0 c]
+node tr c = tree tr [Leaf c]
 
 tree :: Affine -> [Tree] -> Tree
-tree = Parent
+tree = Tree
